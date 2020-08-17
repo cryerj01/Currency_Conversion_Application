422a6236024296e8639f8623cd714ef60c9ff851
Cryerj01 <turne@DESKTOP-QUG8IJR>
2020-08-17 15:19:04 +0100

    Started connecting to database and retreving currency list error in data reader

--- Currency_Conversion_Application/Form1.Designer.cs
+++ Currency_Conversion_Application/Form1.Designer.cs
@@ -32,11 +32,12 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(265, 40);
+            this.button1.Location = new System.Drawing.Point(425, 40);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -62,17 +63,27 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(81, 119);
+            this.comboBox1.Location = new System.Drawing.Point(232, 43);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 3;
             this.comboBox1.Text = "Select one";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select the currency to convert to";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -90,6 +101,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Currency_Conversion_Application/Form1.cs
+++ Currency_Conversion_Application/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,8 +18,34 @@ namespace Currency_Conversion_Application
         public Form1()
         {
             InitializeComponent();
-           
+            getCurrencys();
           
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\turne\\Desktop\\Currency_Conversion_Application\\Currency_Conversion_Application\\Properties\\Database1.mdf;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataReader Dr;
+
+        ComboBox CurrencySelector = new ComboBox();
+        
+
+        private void getCurrencys()
+        {
+            con.Open();
+            string cmdtext = "SELECT CurrenctName FROM Currencys";
+            cmd = new SqlCommand(cmdtext, con);
+            Dr = cmd.ExecuteReader();
+            Dr.Read();
+            int i = 0;
+            while (Dr[i] != null)
+            {
+                CurrencySelector.Items.Add(Dr[i]);
+                i++;
+            }
+
+           
+
+
         }
 
 

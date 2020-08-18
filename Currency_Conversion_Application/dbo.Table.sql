ccb145bfb052897ede1074b9b710b0c2d20087c4
cryerj01 <69801679+cryerj01@users.noreply.github.com>
2020-08-18 18:27:44 +0100

    Delete dbo.Table.sql

--- Currency_Conversion_Application/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL , 
-    [input] CHAR(10) NOT NULL , 
-    [convertedTo] MONEY NOT NULL , 
-    [output] CHAR(10) NOT NULL , 
-    [date] DATE NOT NULL  
-)
